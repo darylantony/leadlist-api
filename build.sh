6e45a3bbaf3a113004d15ae78fb7c7fc670cb5b7
Alexey Kuzminich <akuzminich@gmail.com>
2015-10-09 18:23:27 +0800

    Docker image to build Python wheels for the app and its dependencies

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x;
+set -e;
+mkdir -p wheelhouse;
+
+docker build -t leadlist-base -f base.docker .;
+docker build -t leadlist-builder -f build.docker .;
+docker run --rm \
+       -v "$(pwd)":/application -v "$(pwd)"/wheelhouse:/wheelhouse \
+       leadlist-builder;
+#docker build -t leadlist-run -f run.docker .;
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup (
+    name             = "LeadList",
+    version          = "0.1",
+    description      = "LeadLeast API",
+    packages         = find_packages(),
+    #install_requires = [],
+    #entry_points     = {'console_scripts':
+    #                    ['run-the-app = deployme:main']}
+)
