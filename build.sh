71f7577a329ffa328ffd59448ab6a164cb16955d
Alexey Kuzminich <akuzminich@gmail.com>
2015-10-09 20:00:37 +0800

    Docker image with Django app server with gunicorn

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -e;
 mkdir -p wheelhouse;
 
 docker build -t leadlist-base -f base.docker .;
+
 docker build -t leadlist-builder -f build.docker .;
 docker run --rm \
        -v "$(pwd)":/application -v "$(pwd)"/wheelhouse:/wheelhouse \
        leadlist-builder;
-#docker build -t leadlist-run -f run.docker .;
+
+docker build -t leadlist-run -f run.docker .;
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+exec docker run --rm -it -p 8081:8081 leadlist-run;
+
