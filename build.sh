f90552fbd747bd479e02208b9ee6bc55f55fea2f
Alexey Kuzminich <akuzminich@gmail.com>
2015-10-12 09:45:01 +0800

    Simplify leadlist docker images by using proper existing images

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -x;
 set -e;
-mkdir -p wheelhouse;
+mkdir -p wheels;
 
 docker build -t leadlist-base -f base.docker .;
-
 docker build -t leadlist-builder -f build.docker .;
+
 docker run --rm \
-       -v "$(pwd)":/application -v "$(pwd)"/wheelhouse:/wheelhouse \
+       -v "$(pwd)":/application \
+       -v "$(pwd)"/wheels:/wheels \
        leadlist-builder;
 
 docker build -t leadlist-run -f run.docker .;
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec docker run --rm -it -p 8081:8081 leadlist-run;
+exec docker run --rm -it -p 8000:8000 leadlist-run;
 
